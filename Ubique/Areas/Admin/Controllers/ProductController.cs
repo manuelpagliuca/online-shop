ca1514568f65353ce6a073c0d65e9cdb7e2a72de
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-24 13:21:42 +0200

    Delete API + Warning Alert before removal.

--- Ubique/Areas/Admin/Controllers/ProductController.cs
+++ Ubique/Areas/Admin/Controllers/ProductController.cs
@@ -188,39 +188,6 @@ namespace Ubique.Areas.Admin.Controllers
 			return View();
 		}
 
-		public IActionResult Delete(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-
-			Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-			if (productFromDb == null)
-			{
-				return NotFound();
-			}
-
-			return View(productFromDb);
-		}
-
-		[HttpPost, ActionName("Delete")]
-		public IActionResult DeletePOST(int? id)
-		{
-			Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-
-			if (obj == null)
-			{
-				return NotFound();
-			}
-
-			_unitOfWork.Product.Remove(obj);
-			_unitOfWork.Save();
-			TempData["success"] = "Prodotto rimosso con successo!";
-			return RedirectToAction("Index");
-		}
-
 		#region API CALLS
 
 		[HttpGet]
@@ -234,6 +201,31 @@ namespace Ubique.Areas.Admin.Controllers
 			}
 
 			return Json(new { data = productList });
+		}
+
+		[HttpDelete]
+		public IActionResult Delete(int? id)
+		{
+			var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+
+			if (productToBeDeleted == null)
+			{
+				return Json(new { success = false, message = "Error while deleting" });
+			}
+
+			var oldImagePath = Path.Combine(
+				_webHostEnvironment.WebRootPath,
+				productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+			if (!string.IsNullOrEmpty(oldImagePath) && System.IO.File.Exists(oldImagePath))
+			{
+				System.IO.File.Delete(oldImagePath);
+			}
+
+			_unitOfWork.Product.Remove(productToBeDeleted);
+			_unitOfWork.Save();
+
+			return Json(new { success = true, message = "Delete Successful" });
 		}
 
 		#endregion
--- Ubique/wwwroot/js/product.js
+++ Ubique/wwwroot/js/product.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+$(document).ready(function () {
     loadDataTable();
 });
 
@@ -16,11 +17,34 @@ function loadDataTable() {
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
                      <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a onClick=Delete('/admin/product/delete?id=${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
             }
         ]
     });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Sei sicuro?',
+        text: "Una volta confermato i cambiamenti saranno permanenti!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, rimuovilo!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
 }
