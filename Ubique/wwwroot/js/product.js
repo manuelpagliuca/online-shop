e171a1edc52e7a5037d557765e87321559235bfb
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-24 12:42:15 +0200

    Loading DataTables API for Products.

---
+++ Ubique/wwwroot/js/product.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'name', "width": "25%" },
+            { data: 'brand', "width": "15%" },
+            { data: 'price', "width": "10%" },
+            { data: "subCategory.name", "width": "20%" },
+            { data: 'subCategory.category.name', "width": "15%" }
+        ]
+    });
+}
