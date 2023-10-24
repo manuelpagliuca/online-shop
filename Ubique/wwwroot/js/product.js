6230d48808e3596140e23365a24373553b01eb75
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-24 12:46:35 +0200

    DataTables Edit/Delete option.

--- Ubique/wwwroot/js/product.js
+++ Ubique/wwwroot/js/product.js
@@ -10,7 +10,17 @@ function loadDataTable() {
             { data: 'brand', "width": "15%" },
             { data: 'price', "width": "10%" },
             { data: "subCategory.name", "width": "20%" },
-            { data: 'subCategory.category.name', "width": "15%" }
+            { data: 'subCategory.category.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                     <a class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
         ]
     });
 }
