420ccbd9420d316bec161ee682e034cd6da9fe24
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-25 15:58:51 +0200

    Registration extended with Application User

--- Ubique.DataAccess/Data/ApplicationDbContext.cs
+++ Ubique.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace Ubique.DataAccess.Data
 		public DbSet<Category> Categories { get; set; }
 		public DbSet<SubCategory> SubCategories { get; set; }
 		public DbSet<Product> Products { get; set; }
+		public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
--- Ubique/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ubique/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,26 +2,20 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
-using Microsoft.Extensions.Logging;
+using Ubique.Models;
 
 namespace Ubique.Areas.Identity.Pages.Account
 {
-    public class RegisterModel : PageModel
+	public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
@@ -154,11 +148,11 @@ namespace Ubique.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
