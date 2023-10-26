86b4f0ebbfd2c69f808d27de682312ffbf7827d3
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-26 09:35:00 +0200

    Add Role Authorization to controller actions
    + Added Configuration for Application Cookies

--- Ubique/Areas/Admin/Controllers/CategoryController.cs
+++ Ubique/Areas/Admin/Controllers/CategoryController.cs
@@ -1,111 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Ubique.DataAccess.Repository.IRepository;
 using Ubique.Models;
+using Ubique.Utility;
 
 namespace Ubique.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class CategoryController : Controller
-    {
-        private readonly IUnitOfWork _unitOfWork;
+	[Area("Admin")]
+	[Authorize(Roles = StaticDetails.Role_Admin)]
+	public class CategoryController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+		public CategoryController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-            List<Category> categories = _unitOfWork.Category.GetAll().ToList();
-            return View(categories);
-        }
+		public IActionResult Index()
+		{
+			List<Category> categories = _unitOfWork.Category.GetAll().ToList();
+			return View(categories);
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Create(Category obj)
-        {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("Name", "Il campo \"Ordine Visualizzazione\" non può essere uguale al campo \"Nome Categoria\".");
-            }
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("Name", "Il campo \"Ordine Visualizzazione\" non può essere uguale al campo \"Nome Categoria\".");
+			}
 
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Category.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Categoria creata con successo!";
-                return RedirectToAction("Index", "Category");
-            }
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Category.Add(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Categoria creata con successo!";
+				return RedirectToAction("Index", "Category");
+			}
 
-            return View();
-        }
+			return View();
+		}
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public IActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            Category? fromDb = _unitOfWork.Category.Get(u => u.Id == id);
+			Category? fromDb = _unitOfWork.Category.Get(u => u.Id == id);
 
-            if (fromDb == null)
-            {
-                return NotFound();
-            }
+			if (fromDb == null)
+			{
+				return NotFound();
+			}
 
-            return View(fromDb);
-        }
+			return View(fromDb);
+		}
 
-        [HttpPost]
-        public IActionResult Edit(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Category.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Categoria aggiornata con successo!";
-                return RedirectToAction("Index", "Category");
-            }
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Category.Update(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Categoria aggiornata con successo!";
+				return RedirectToAction("Index", "Category");
+			}
 
-            return View();
-        }
+			return View();
+		}
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
 
-            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+			Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
 
-            if (categoryFromDb == null)
-            {
-                return NotFound();
-            }
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
 
-            return View(categoryFromDb);
-        }
+			return View(categoryFromDb);
+		}
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
 
-            if (obj == null)
-            {
-                return NotFound();
-            }
+			if (obj == null)
+			{
+				return NotFound();
+			}
 
-            _unitOfWork.Category.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Categoria rimossa con successo!";
-            return RedirectToAction("Index");
-        }
-    }
+			_unitOfWork.Category.Remove(obj);
+			_unitOfWork.Save();
+			TempData["success"] = "Categoria rimossa con successo!";
+			return RedirectToAction("Index");
+		}
+	}
 }
