25f17aa2d6197150337b61da6599d0298d48d450
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-27 10:55:53 +0200

    Adding ShoppingCart model to the DB.

--- Ubique.DataAccess/Data/ApplicationDbContext.cs
+++ Ubique.DataAccess/Data/ApplicationDbContext.cs
@@ -17,8 +17,10 @@ namespace Ubique.DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
