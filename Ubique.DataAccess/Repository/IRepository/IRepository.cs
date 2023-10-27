09d9fc86f1b13d0468ba8e1a66d4abbb0d44da10
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-27 14:58:29 +0200

    Get Price Order total
    + BoilerPlate code for handling the case price base on item count.

--- Ubique.DataAccess/Repository/IRepository/IRepository.cs
+++ Ubique.DataAccess/Repository/IRepository/IRepository.cs
@@ -4,7 +4,7 @@ namespace Ubique.DataAccess.Repository.IRepository
 {
 	public interface IRepository<T> where T : class
 	{
-		IEnumerable<T> GetAll(string? includeProperties = null);
+		IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
 		T Get(Expression<Func<T, bool>> filter, bool tracked = false, string? includeProperties = null);
 		IEnumerable<T> GetList(Expression<Func<T, bool>> filter);
 		void Add(T entity);
--- Ubique/Areas/Customer/Controllers/CartController.cs
+++ Ubique/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Ubique.DataAccess.Repository.IRepository;
+using Ubique.Models;
+using Ubique.Models.ViewModels;
 
 namespace Ubique.Areas.Customer.Controllers
 {
-	[Area("customer")]
+	[Area("Customer")]
+	[Authorize]
 	public class CartController : Controller
 	{
+		private readonly IUnitOfWork _unitOfWork;
+		public ShoppingCartVM ShoppingCartVM { get; set; }
+		public CartController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
 		public IActionResult Index()
 		{
-			return View();
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+			ShoppingCartVM = new()
+			{
+				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+				includeProperties: "Product")
+			};
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartList)
+			{
+				double price = GetPriceBasedOnQuantity(cart); // if the price change given the item count
+				ShoppingCartVM.OrderTotal += (price * cart.Count);
+			}
+
+			return View(ShoppingCartVM);
+		}
+
+
+		private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+		{
+			return shoppingCart.Product.Price;
+			// TODO: maybe could be needed in the future, give different price values
+			//if (shoppingCart.Count <= 50)
+			//{
+			//	return shoppingCart.Product.Price;
+			//}
+			//else
+			//{
+			//	if (shoppingCart.Count <= 100)
+			//	{
+			//		return shoppingCart.Product.Price50;
+			//	}
+			//	else
+			//	{
+			//		return shoppingCart.Product.Price100;
+			//	}
+			//}
 		}
 	}
 }
