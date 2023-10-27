a13f0199bb1c73154df9f38c63ff67c50db0c9db
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-27 15:31:12 +0200

    Update Quantity and price in shopping cart view.

--- Ubique/Areas/Customer/Controllers/CartController.cs
+++ Ubique/Areas/Customer/Controllers/CartController.cs
@@ -32,12 +32,47 @@ namespace Ubique.Areas.Customer.Controllers
 			foreach (var cart in ShoppingCartVM.ShoppingCartList)
 			{
 				double price = GetPriceBasedOnQuantity(cart); // if the price change given the item count
+				cart.Price = price; 
 				ShoppingCartVM.OrderTotal += (price * cart.Count);
 			}
 
 			return View(ShoppingCartVM);
 		}
 
+		public IActionResult Plus(int cartId)
+		{
+			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+			cartFromDb.Count += 1;
+			_unitOfWork.ShoppingCart.Update(cartFromDb);
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Minus(int cartId)
+		{
+			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+			if (cartFromDb.Count <= 1)
+			{
+				//remove that from cart
+				_unitOfWork.ShoppingCart.Remove(cartFromDb);
+			}
+			else
+			{
+				cartFromDb.Count -= 1;
+				_unitOfWork.ShoppingCart.Update(cartFromDb);
+			}
+
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Remove(int cartId)
+		{
+			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+			_unitOfWork.ShoppingCart.Remove(cartFromDb);
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
 
 		private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
 		{
