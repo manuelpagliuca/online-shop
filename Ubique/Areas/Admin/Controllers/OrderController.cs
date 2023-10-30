1c5b8456fec99761a99991369ab0d3679b91facf
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-10-30 14:56:28 +0100

    Update Order Details

--- Ubique/Areas/Admin/Controllers/OrderController.cs
+++ Ubique/Areas/Admin/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Ubique.DataAccess.Repository.IRepository;
 using Ubique.Models;
@@ -12,6 +13,9 @@ namespace Ubique.Areas.Admin.Controllers
 	{
 		private readonly IUnitOfWork _unitOfWork;
 
+		[BindProperty]
+		public OrderVM OrderVM { get; set; }
+
 		public OrderController(IUnitOfWork unitOfWork)
 		{
 			_unitOfWork = unitOfWork;
@@ -24,13 +28,13 @@ namespace Ubique.Areas.Admin.Controllers
 
 		public IActionResult Details(int orderId)
 		{
-			OrderVM orderVM = new()
+			OrderVM = new()
 			{
 				OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
 				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
 			};
 
-			return View(orderVM);
+			return View(OrderVM);
 		}
 
 
@@ -62,7 +66,40 @@ namespace Ubique.Areas.Admin.Controllers
 
 			return Json(new { data = objOrderHeaders });
 
-			#endregion
 		}
+
+		[HttpPost]
+		[Authorize(Roles = StaticDetails.Role_Admin + "," + StaticDetails.Role_Employee)]
+		public IActionResult UpdateOrderDetail()
+		{
+			var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+
+			orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+			orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+			orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+			orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+			orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+			orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+
+			if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
+			{
+				orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+			}
+
+			if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+			{
+				orderHeaderFromDb.Carrier = OrderVM.OrderHeader.TrackingNumber;
+			}
+
+			_unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+			_unitOfWork.Save();
+
+			TempData["Success"] = "Order Details Updated Successfully.";
+
+
+			return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
+		}
+
+		#endregion
 	}
 }
