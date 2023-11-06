029e18f14a7235a25c2edc4fc987e72ec019c3d7
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2023-11-06 12:32:10 +0100

    Dynamic Role Management Logic

--- Ubique/Areas/Admin/Controllers/UserController.cs
+++ Ubique/Areas/Admin/Controllers/UserController.cs
@@ -1,79 +1,133 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Ubique.DataAccess.Data;
 using Ubique.DataAccess.Repository.IRepository;
 using Ubique.Models;
+using Ubique.Models.ViewModels;
 using Ubique.Utility;
 
 namespace Ubique.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	[Authorize(Roles = StaticDetails.Role_Admin)]
-	public class UserController : Controller
-	{
-		private readonly IUnitOfWork _unitOfWork;
-		private readonly ApplicationDbContext _db;
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
 
-		public UserController(IUnitOfWork unitOfWork, ApplicationDbContext db)
-		{
-			_unitOfWork = unitOfWork;
-			_db = db;
-		}
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RoleManagment(string userId)
+        {
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
 
-		#region API CALLS
+            RoleManagmentVM RoleVM = new RoleManagmentVM()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+                RoleList = _db.Roles.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = _db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
 
-		[HttpGet]
-		public IActionResult GetAll()
-		{
-			List<ApplicationUser> userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
-			var userRole = _db.UserRoles.ToList();
-			var roles = _db.Roles.ToList();
-			// if not a company user
-			foreach (var user in userList)
-			{
-				var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
-				user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+            RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+            return View(RoleVM);
+        }
 
-				if (user.Company == null)
-				{
-					user.Company = new() { Name = "" };
-				}
-			}
+        #region API CALLS
+
+        [HttpPost]
+        public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM)
+        {
+
+            string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+            if (!(roleManagmentVM.ApplicationUser.Role == oldRole))
+            {
+                //a role was updated
+                ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+                if (roleManagmentVM.ApplicationUser.Role == StaticDetails.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+                }
+                if (oldRole == StaticDetails.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+                _db.SaveChanges();
+
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            // if not a company user
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
 
 
-			return Json(new { data = userList });
-		}
+            return Json(new { data = userList });
+        }
 
-		[HttpPost]
-		public IActionResult LockUnlock([FromBody] string? id)
-		{
-			var fromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string? id)
+        {
+            var fromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
 
-			if (fromDb == null)
-			{
-				return Json(new { success = false, message = "Error while Locking/Unlocking" });
-			}
+            if (fromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
 
-			if (fromDb.LockoutEnd != null && fromDb.LockoutEnd > DateTime.Now)
-			{
-				// user is currently locked and it needs to be unlocked
-				fromDb.LockoutEnd = DateTime.Now;
-			}
-			else
-			{
-				fromDb.LockoutEnd = DateTime.Now.AddYears(1000);
-			}
+            if (fromDb.LockoutEnd != null && fromDb.LockoutEnd > DateTime.Now)
+            {
+                // user is currently locked and it needs to be unlocked
+                fromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                fromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
 
-			_db.SaveChanges();
+            _db.SaveChanges();
 
-			return Json(new { success = true, message = "Operazione Compiuta" });
-		}
+            return Json(new { success = true, message = "Operazione Compiuta" });
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
--- Ubique/wwwroot/js/user.js
+++ Ubique/wwwroot/js/user.js
@@ -14,33 +14,37 @@ function loadDataTable() {
             { "data": "company.name", "width": "15%" },
             { "data": "role", "width": "15%" },
             {
-                data: { id: 'id', lockoutEnd: 'lockoutEnd' },
+                data: { id: "id", lockoutEnd: "lockoutEnd" },
                 "render": function (data) {
                     var today = new Date().getTime();
                     var lockout = new Date(data.lockoutEnd).getTime();
 
                     if (lockout > today) {
-                        return `<div class="text-center">
-                                    <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
-                                        <i class="bi bi-unlock-fill"></i> UnLock
-                                    </a>
-                                    <a class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
-                                        <i class="bi bi-pencil-square"></i> Permission
-                                    </a>
-                                </div>
-                        `;
+                        return `
+                        <div class="text-center">
+                             <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                    <i class="bi bi-lock-fill"></i> Lock
+                                </a> 
+                                <a href="/Admin/User/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                     <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                        </div>
+                    `
                     }
                     else {
-                        return `<div class="text-center">
-                                    <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
-                                        <i class="bi bi-unlock-fill"></i> UnLock
-                                    </a>
-                                    <a class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
-                                        <i class="bi bi-pencil-square"></i> Permission
-                                    </a>
-                                </div>
-                        `;
+                        return `
+                        <div class="text-center">
+                              <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                    <i class="bi bi-unlock-fill"></i> UnLock
+                                </a>
+                                <a href="/Admin/User/RoleManagment?userId=${data.id}" class="btn btn-danger text-white" style="cursor:pointer; width:150px;">
+                                     <i class="bi bi-pencil-square"></i> Permission
+                                </a>
+                        </div>
+                    `
                     }
+
+
                 },
                 "width": "25%"
             }
